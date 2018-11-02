abc90e7a9a81c9b74763aa4bc21731f5103e8e38
David Zeng <davidzeng.42@gmail.com>
2018-11-01 23:56:54 -0400

    minor

--- baseline/visdial/encoders/hre2.py
+++ baseline/visdial/encoders/hre2.py
@@ -96,7 +96,7 @@ class HierarchicalRecurrentEncoder(nn.Module):
         fused_embedding = fused_embedding.permute(1, 0, 2)
         output, final = self.dialog_rnn(fused_embedding, None)
 
-        # LSTM outputs tensor with dimensions (seq_len, batch, hidden_size)
+        # LSTM outputs tensor with dimensions (seq_len, batch, rnn_hidden_size)
         output = output.permute(1, 0, 2)
         output = output.contiguous().view(-1, output.size(2))
         output = torch.tanh(output)
