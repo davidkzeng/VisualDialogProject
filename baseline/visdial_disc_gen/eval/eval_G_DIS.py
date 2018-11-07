2dbbea8295b0ab19ca366347a504d27d725c8460
Ubuntu <ubuntu@ip-172-31-17-11.us-east-2.compute.internal>
2018-11-07 01:17:03 +0000

    Fix eval and sampling code

--- baseline/visdial_disc_gen/eval/eval_G_DIS.py
+++ baseline/visdial_disc_gen/eval/eval_G_DIS.py
@@ -143,13 +143,6 @@ def eval():
             ans = opt_answer[:,rnd,:,:].clone().view(-1, ans_length).t()
             gt_id = answer_ids[:,rnd]
 
-            if i == 0 and rnd == 0:
-                print("ques",question[:,rnd,:])
-                print("his",history[:,:rnd+1,:])
-                print("tans",opt_answerT[:,rnd,:])
-                print("ans",opt_answer[:,rnd,:,:])
-                print("gt_id",gt_id)
-
             his_input.data.resize_(his.size()).copy_(his)
             ques_input.data.resize_(ques.size()).copy_(ques)
             ans_input.data.resize_(ans.size()).copy_(ans)
@@ -202,10 +195,23 @@ def eval():
             rank_all_tmp += list(rank.view(-1).data.cpu().numpy())
 
             if i == 0 and rnd == 0:
+                print("gt_score size",gt_score.size())
                 print("gt_score",gt_score)
+                print("sort_score size",sort_score.size())
                 print("sort_score",sort_score)
+                print("count size",count.size())
                 print("count",count)
-                print("rank_all_tmp",rank_all_tmp)
+                print("rank size",rank.view(-1).size())
+                print("rank",rank.view(-1))
+                print("tans size", opt_answerT[:,rnd,:,:].clone().view(-1,
+                    ans_length).t().size())
+                print("opt_answerT size", opt_answerT.size())
+
+            ques_txt = decode_txt(itow, questionL[:,rnd,:].t())
+            for j in range(batch_size):
+                tmp_tans = opt_answerT[j,rnd,int(gt_id[j]),:]
+                tans_txt = decode_txt(itow, tmp_tans.view(tmp_tans.size()[0],1))
+                print('Q: %s --A: %s' % (ques_txt[j], tans_txt[0]))
 
         i += 1
         sys.stdout.write('Evaluating: {:d}/{:d}  \r' \
@@ -232,10 +238,15 @@ def sample():
     hist_hidden = netE.init_hidden(opt.batchSize)
 
     i = 0
+    rank_color = []
+    rank_count = []
+    rank_yn = []
+    rank_round = [[], [], [], [], [], [], [], [], [], []]
     while i < len(dataloader_val):
         data = data_iter_val.next()
         image, history, question, answer, answerT, questionL, opt_answer, \
                 opt_answerT, answer_ids, answerLen, opt_answerLen, img_id  = data
+        img_id_data = img_id.data.cpu().numpy()
 
         batch_size = question.size(0)
         image = image.view(-1, 512)
@@ -263,8 +274,34 @@ def sample():
 
             encoder_feat, ques_hidden = netE(ques_emb, his_emb, img_input, \
                                                 ques_hidden, hist_hidden, rnd+1)
-
             _, ques_hidden = netG(encoder_feat.view(1,-1,opt.ninp), ques_hidden)
+
+            hidden_replicated = []
+            for hid in ques_hidden:
+                hidden_replicated.append(hid.view(opt.nlayers, batch_size, 1, \
+                    opt.nhid).expand(opt.nlayers, batch_size, 100, opt.nhid).clone().view(opt.nlayers, -1, opt.nhid))
+            hidden_replicated = tuple(hidden_replicated)
+
+            ans_emb = netW(ans_input, format = 'index')
+
+            output, _ = netG(ans_emb, hidden_replicated)
+            logprob = - output
+            logprob_select = torch.gather(logprob, 1, ans_target.view(-1,1))
+
+            mask = ans_target.data.eq(0)  # generate the mask
+            if isinstance(logprob, Variable):
+                mask = Variable(mask, volatile=logprob.volatile)
+            logprob_select.masked_fill_(mask.view_as(logprob_select), 0)
+
+            prob = logprob_select.view(ans_length, -1, 100).sum(0).view(-1,100)
+
+            for b in range(batch_size):
+                gt_index.data[b] = gt_index.data[b] + b*100
+
+            gt_score = prob.view(-1).index_select(0, gt_index)
+            sort_score, sort_idx = torch.sort(prob, 1)
+            count = sort_score.lt(gt_score.view(-1,1).expand_as(sort_score))
+            rank = count.sum(1) + 1
 
             # ques_emb = embeder(ques_input)
             # hidden = repackage_hidden(hidden, batch_size)
@@ -292,20 +329,57 @@ def sample():
                 ans_sample.data.copy_(idx.data)
                 ans_sample_result[t].copy_(ans_sample.view(batch_size).data)
 
+            # print("ans_sample_result", ans_sample_result)
             # print("ans_sample_result size", ans_sample_result.size())
             # print("tans size", tans.size())
             # print("gt_id size", gt_id.size())
             # print("ques size", questionL[:,rnd,:].t().size())
             ans_sample_txt = decode_txt(itow, ans_sample_result)
-            ans_txt = decode_txt(itow, tans)
             ques_txt = decode_txt(itow, questionL[:,rnd,:].t())
-            # print("len(ans_txt)", len(ans_txt))
-            for j in range(len(ans_sample_txt)):
-                print('Q: %s --A: %s --Sampled: %s' %(ques_txt[j],
-                    ans_txt[int(gt_id[j])], ans_sample_txt[j]))
+            rank_data = rank.data.cpu().numpy()
+            rank_round[rnd] += list(rank_data)
+            for j in range(batch_size):
+                tmp_tans = opt_answerT[j,rnd,int(gt_id[j]),:]
+                top_ans = opt_answerT[j,rnd,int(sort_idx[j][0]),:]
+                tans_txt = decode_txt(itow, tmp_tans.view(tmp_tans.size()[0],1))
+                top_ans_txt = decode_txt(itow, top_ans.view(top_ans.size()[0],1))
 
-            pdb.set_trace()
+                if "color" in ques_txt[j]:
+                    rank_color += [rank_data[j]]
+                if "how many" in ques_txt[j] or any(char.isdigit() for char in tans_txt[0]):
+                    rank_count += [rank_data[j]]
+                if "yes" in tans_txt[0] or "no" in tans_txt[0]:
+                    rank_yn += [rank_data[j]]
+
+                if (rnd == 0 or rnd == 9) and i % 10 == 0:
+                    print('img_id: %d Q: %s --A: %s -- Sampled from candidate answers: %s rank of A: %d, Sampled: %s'
+                        %(img_id_data[j], ques_txt[j], tans_txt[0], top_ans_txt[0], rank_data[j], ans_sample_txt[j]))
+
+            # pdb.set_trace()
         i += 1
+        sys.stdout.write('Evaluating: {:d}/{:d}  \r' \
+          .format(i, len(dataloader_val)))
+
+        if i % 50 == 0:
+            for (name,rank_tmp) in [("color",rank_color),("count",rank_count),("yes/no",rank_yn)]:
+                R1 = np.sum(np.array(rank_tmp)==1) / float(len(rank_tmp))
+                R5 =  np.sum(np.array(rank_tmp)<=5) / float(len(rank_tmp))
+                R10 = np.sum(np.array(rank_tmp)<=10) / float(len(rank_tmp))
+                ave = np.sum(np.array(rank_tmp)) / float(len(rank_tmp))
+                mrr = np.sum(1/(np.array(rank_tmp, dtype='float'))) / float(len(rank_tmp))
+                print ('%d/%d %s: mrr: %f R1: %f R5 %f R10 %f Mean %f' %(i,
+                    len(dataloader_val), name, mrr, R1, R5, R10, ave))
+            for (r,rank_tmp) in enumerate(rank_round):
+                if len(rank_tmp) > 0:
+                    R1 = np.sum(np.array(rank_tmp)==1) / float(len(rank_tmp))
+                    R5 =  np.sum(np.array(rank_tmp)<=5) / float(len(rank_tmp))
+                    R10 = np.sum(np.array(rank_tmp)<=10) / float(len(rank_tmp))
+                    ave = np.sum(np.array(rank_tmp)) / float(len(rank_tmp))
+                    mrr = np.sum(1/(np.array(rank_tmp, dtype='float'))) / float(len(rank_tmp))
+                    print ('%d/%d, round %d: mrr: %f R1: %f R5 %f R10 %f Mean %f' %(i,
+                        len(dataloader_val), r, mrr, R1, R5, R10, ave))
+
+    return rank_color, rank_count, rank_yn, rank_round
 
 ####################################################################################
 # Main
